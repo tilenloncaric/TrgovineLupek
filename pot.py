78f729c7aad3c58f9a99719d968efb073ed65e16
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-15 13:03:58 +0200

    Add files via upload

---
+++ pot.py
@@ -0,0 +1,7 @@
+import os               # delo s datotekami in mapami
+
+
+def pot_do_mape():
+    '''funkcija, ki vrne absolutno pot mape TrgovineLupek, pot v kateri se nahaja naša datoteka'''
+    
+    return os.path.dirname(os.path.abspath(__file__))

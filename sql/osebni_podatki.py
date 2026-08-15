fd12bb527bae91d479071c3d40aeb33127512938
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-15 19:29:29 +0200

    Refactor osebni_podatki function in osebni_podatki.py

--- sql/osebni_podatki.py
+++ sql/osebni_podatki.py
@@ -5,6 +5,6 @@ def osebni_podatki(vnesen_id_zaposlenega):
     '''prikaže osebne podatke zaposlenega'''
 
     sql =   """ SELECT zaposleni.ime, zaposleni.priimek, zaposleni.spol, zaposleni.delovno_mesto, poslovalnice.ime FROM zaposleni 
-                    JOIN poslovanice ON zaposleni.id_poslovanice = poslovanice.id_poslovanice
+                    JOIN poslovalnice ON zaposleni.id_poslovalnice = poslovalnice.id_poslovalnice
                         WHERE zaposleni.id_zaposlenega = vnesen_id_zaposlenega 
             """

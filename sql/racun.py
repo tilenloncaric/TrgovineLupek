69c2ca26a32cb9c8a0e3a6552a5d7301363a9c89
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-15 20:01:03 +0200

    Refactor SQL queries and improve readability

--- sql/racun.py
+++ sql/racun.py
@@ -11,10 +11,12 @@ def ime_tabele(leto_izdaje):
 
 def poslovalnica(stevilka_racuna, leto_izdaje):
     '''vrne poslovalnico, v kateri je bil izdan račun'''
+      prodaja = ime_tabele(leto_izdaje)
 
     sql =   SELECT poslovalnice.ime FROM poslovalnice
-                JOIN tabela_s_podatki ON poslovalnice.id_poslovalnice = tabela_s_podatki.id_poslovalnice
-                    WHERE tabela_s_podatki.id_poslovalnice = stevilka_racuna    
+                JOIN prodaja ON poslovalnice.id_poslovalnice = prodaja.id_poslovalnice
+                    WHERE prodaja.id_racuna = stevilka_racuna  
+                        GROUP BY poslovalnice.ime
 
 
 def skupni_sestevek_prodaje(stevilka_racuna, leto_izdaje):
@@ -27,21 +29,22 @@ sql = SELECT SUM(prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
       WHERE prodaja.id_racuna = stevilka_racuna
 
 def prodani_izdelki(stevilka_racuna, leto_izdaje):
-    '''vrne seznam prodanih izdelkov in prodano količino posameznega izdelka za izbran račun'''
+    '''vrne seznam prodanih izdelkov, prodano količino in vmesno ceno posameznega izdelka za izbran račun'''
 
     prodaja = ime_tabele(leto_izdaje)
 
     SELECT prodaja.izdelek, prodaja.kolicina, (prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
       JOIN izdelki ON prodaja.izdelek = izdelki.ime
-        WHERE id_racuna = stevilka_racuna
+        WHERE prodaja.id_racuna = stevilka_racuna
 
 
 def blagajnik(stevilka_racuna, leto_izdaje):
-    '''vrne blagajnika, ki je izdal račun'''
+    '''vrne ime blagajnika, ki je izdal račun'''
       prodaja = ime_tabele(leto_izdaje)
-      SELECT id_prodajalca FROM prodaja
-            WHERE id_racuna = stevilka_racuna
-                  GROUP BY id_prodajalca
+      SELECT zaposleni.ime, zaposleni.priimek FROM zaposleni
+            JOIN prodaja ON prodaja.id_prodajalca = zaposleni.id_zaposlenega
+                  WHERE prodaja.id_racuna = stevilka_racuna
+                        GROUP BY zaposleni.ime
 
 def datum_izdaje(stevilka_racuna, leto_izdaje):
     '''vrne datum izdaje računa'''
@@ -57,4 +60,5 @@ def podatki_poslovalnice(stevilka_racuna, leto_izdaje):
       SELECT poslovalnice.ime, poslovalnice.delovni_cas, poslovalnice.postna_stevilka, poslovalnice.kraj, poslovalnice.naslov, poslovalnice.telefon FROM poslovalnice
             JOIN prodaja ON poslovalnice.id_poslovalnice = prodaja.id_poslovalnice
                   WHERE prodaja.id_racuna = stevilka_racuna
+                        GROUP BY poslovalnice.ime
       

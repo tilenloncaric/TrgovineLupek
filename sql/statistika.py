8b8ab4a3cd729b9ffadd1be2fd5f37685f52801d
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 18:31:39 +0200

    Refactor SQL functions to include database path

--- sql/statistika.py
+++ sql/statistika.py
@@ -1,16 +1,23 @@
 def tabela(izbrano_leto):
   return f'prodaja{izbrano_leto}'
 
-def poslovalnica_uporabnika(vpisan_id_uporabnika):
+def poslovalnica_uporabnika(vpisan_id_uporabnika, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
   sql = f"""SELECT id_poslovalnice FROM zaposleni
     WHERE id_zaposlenega = vpisan_id_uporabnika
 """
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
 poslovalnica = poslovalnica_uporabnika(vpisan_id_uporabnika)
     
-def sestevek_letne_prodaje(poslovalnica, izbrano_leto):
+def sestevek_letne_prodaje(poslovalnica, izbrano_leto, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -20,7 +27,14 @@ def sestevek_letne_prodaje(poslovalnica, izbrano_leto):
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
 """
-def top10(poslovalnica, izbrano_leto):
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+def top10(poslovalnica, izbrano_leto, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -31,7 +45,14 @@ def top10(poslovalnica, izbrano_leto):
         ORDER BY SUM(kolicina) DESC
           LIMIT 10
 """
-def najslabsih10(poslovalnica, izbrano_leto):
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+
+def najslabsih10(poslovalnica, izbrano_leto, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -42,7 +63,14 @@ def najslabsih10(poslovalnica, izbrano_leto):
         ORDER BY SUM(kolicina) ASC
           LIMIT 10
 """
-def mesecna_prodaja_v_izbranem_letu(poslovalnica, izbrano_leto):     
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+def mesecna_prodaja_v_izbranem_letu(poslovalnica, izbrano_leto, pot_do_baze):     
   TREBA ŠE POPRAVIT
 povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
@@ -53,7 +81,14 @@ povezava_na_bazo = sqlite3.connect(pot_do_baze)
       WHERE prodaja.id_poslovalnice = poslovalnica
   """
 
-def nabavna_cena_v_letu((poslovalnica, izbrano_leto):
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+
+def nabavna_cena_v_letu((poslovalnica, izbrano_leto, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -62,10 +97,18 @@ def nabavna_cena_v_letu((poslovalnica, izbrano_leto):
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
 """
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+
 def ustvarjen_profit(poslovalnica, izbrano_leto):
   return sestevek_letne_prodaje(poslovalnica, izbrano_leto) - nabavna_cena_v_letu((poslovalnica, izbrano_leto)
 
-def najboljsi_dan_prodaje(poslovalnica, izbrano_leto):
+def najboljsi_dan_prodaje(poslovalnica, izbrano_leto, pot_do_baze):
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -77,7 +120,14 @@ def najboljsi_dan_prodaje(poslovalnica, izbrano_leto):
           ORDER BY dnevni_prihodek DESC
             LIMIT 1
 """
-def najslabsi_dan_prodaje(poslovalnica, izbrano_leto):
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+def najslabsi_dan_prodaje(poslovalnica, izbrano_leto, pot_do_baze):
   
   povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
@@ -90,7 +140,15 @@ def najslabsi_dan_prodaje(poslovalnica, izbrano_leto):
           ORDER BY dnevni_prihodek ASC
             LIMIT 1
 """
-def stevilo_prodaj(poslovalnica, izbrano_leto):  group by se je potrebno
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+
+def stevilo_prodaj(poslovalnica, izbrano_leto, pot_do_baze):  group by se je potrebno
 povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
 
@@ -98,7 +156,14 @@ povezava_na_bazo = sqlite3.connect(pot_do_baze)
   sql = f"""SELECT COUNT(DISTINCT id_racuna) FROM prodaja
     WHERE id_poslovalnice = poslovalnica
 """
-def napoved_prodaje(poslovalnica, izbran_izdelek):     
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat
+def napoved_prodaje(poslovalnica, izbran_izdelek, pot_do_baze):     
   DODATI JE ŠE TREBA V KATERI POSLOVALNICI NAJ GLEDA
 povezava_na_bazo = sqlite3.connect(pot_do_baze)
     cursor = povezava_na_bazo.cursor()
@@ -110,3 +175,10 @@ povezava_na_bazo = sqlite3.connect(pot_do_baze)
   SELECT kolicina2024, kolicina2025, (kolicina2025 * (1 + (kolicina2025 - kolicina2024) / kolicina2024)) FROM leto2024
     CROSS JOIN leto2025
   """
+
+cursor.execute(sql, (stevilka_racuna))  
+      rezultat = cursor.fetchone()
+
+      povezava_na_bazo.close()         # zapre povezavo z bazo
+
+      return rezultat

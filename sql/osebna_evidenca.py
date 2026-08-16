de2596697f9e6ded9a44bbb5ed81782a52d8b240
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 15:35:25 +0200

    Update functions to accept database path
    Refactor functions to include database path parameter and improve SQL queries.

--- sql/osebna_evidenca.py
+++ sql/osebna_evidenca.py
@@ -2,70 +2,100 @@ import sqlite3
 
 
 def tabela(izbrano_leto):
-    '''vrne ime tabele, ki vsebuje podatke o evidenci zaposlenega za izbrano leto'''
-
-    tabela_s_podatki = f'evidenca{izbrano_leto}'  
-
-    return tabela_s_podatki
+    '''vrne ime tabele (kot niz), ki vsebuje podatke o evidenci zaposlenega za izbrano leto'''
+    return f'evidenca{izbrano_leto}'
 
 
-def oddelane_ure_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+def datum(izbran_mesec, izbrano_leto):
+    '''vrne datum (kot niz)'''
+    return f'%{izbran_mesec}.{izbrano_leto}'
+
+
+def oddelane_ure_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
     '''sešteje oddelane ure zaposlenega v izbranem mesecu in letu'''
 
-    izbran_datum = f'%{izbran_mesec}.{izbrano_leto}'
-    evidenca = tabela(izbrano_leto)
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor()
 
-    sql =   """ SELECT SUM(strftime('%s', cas_odhoda) - strftime('%s', cas_prihoda)) FROM evidenca
-                    WHERE datum LIKE izbran_datum
+    sql =  f""" SELECT SUM(strftime('%s', cas_odhoda) - strftime('%s', cas_prihoda)) FROM {tabela(izbrano_leto)}
+                    WHERE datum LIKE ?
                             AND 
-                        id_zaposlenega = vnesen_id_zaposlenega
+                        id_zaposlenega = ?
             """
-    izpisano v sekundah naj pravilno vrača
+    
+    cursor.execute(sql, (datum(izbran_mesec, izbrano_leto), vnesen_id_zaposlenega)) 
+    rezultat = cursor.fetchone()
+
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+    return rezultat
                             
                             
-def oddelane_ure_za_posamezen_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+def oddelane_ure_za_posamezen_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
+    
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor()
 
-    izbran_datum = f'%{izbran_mesec}.{izbrano_leto}'
-    evidenca = tabela(izbrano_leto)
-
-    sql =   """ SELECT datum, (strftime('%s', cas_odhoda) - strftime('%s', cas_prihoda)) FROM evidenca
-                    WHERE datum LIKE izbran_datum
+    sql =  f""" SELECT datum, (strftime('%s', cas_odhoda) - strftime('%s', cas_prihoda)) FROM {tabela(izbrano_leto)}
+                    WHERE datum LIKE ?
                             AND 
-                        id_zaposlenega = vnesen_id_zaposlenega
+                        id_zaposlenega = ?
             """
+    
+    cursor.execute(sql, (datum(izbran_mesec, izbrano_leto), vnesen_id_zaposlenega))  # potrebna vejica, da se naredi tuple, sicer ne dela?????
+    rezultat = cursor.fetchall()
 
+    povezava_na_bazo.close()         # zapre povezavo z bazo
 
-def delovna_obveznost_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+    return rezultat
+    
 
-    izbran_datum = f'%{izbran_mesec}.{izbrano_leto}'
-    evidenca = tabela(izbrano_leto)
+def delovna_obveznost_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
+    
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor()
 
-    sql =   """ SELECT SUM(delovna_obveznost) * 3600 FROM evidenca
-                    WHERE datum LIKE izbran_datum
+    sql =  f""" SELECT SUM(delovna_obveznost) * 3600 FROM {tabela(izbrano_leto)}
+                    WHERE datum LIKE ?
                             AND 
-                        id_zaposlenega = vnesen_id_zaposlenega
+                        id_zaposlenega = ?
             """
+    
+    cursor.execute(sql, (datum(izbran_mesec, izbrano_leto), vnesen_id_zaposlenega))  # potrebna vejica, da se naredi tuple, sicer ne dela?????
+    rezultat = cursor.fetchone()
 
+    povezava_na_bazo.close()         # zapre povezavo z bazo
 
-def delovna_obveznost_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+    return rezultat
+    
 
-    izbran_datum = f'%{izbran_mesec}.{izbrano_leto}'
-    evidenca = tabela(izbrano_leto)
+def delovna_obveznost_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
+    
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor()
 
-    sql =   """ SELECT datum, delovna_obveznost FROM evidenca
-                    WHERE datum LIKE izbran_datum
+    sql =  f""" SELECT datum, delovna_obveznost FROM {tabela(izbrano_leto)}
+                    WHERE datum LIKE ?
                             AND 
-                        id_zaposlenega = vnesen_id_zaposlenega
+                        id_zaposlenega = ?
             """
+    
+    cursor.execute(sql, (datum(izbran_mesec, izbrano_leto), vnesen_id_zaposlenega))  # potrebna vejica, da se naredi tuple, sicer ne dela?????
+    rezultat = cursor.fetchall()
+
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+    return rezultat
+    
 
 
-def razlika_mesec(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+# to v glavni kodi samo naredi, ne rabiš dvojnega tukaj
+def razlika_mesec(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
     '''izračuna razliko med oddelanimi urami in delovno obveznostjo zaposlenega v izbranem mesecu in letu'''
+    return oddelane_ure_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze) - delovna_obveznost_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze)
 
-    razlika = oddelane_ure_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto) - delovna_obveznost_v_mesecu(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto)
 
-
-def razlika_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto):
+def razlika_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto, pot_do_baze):
     '''izračuna razliko med oddelanimi urami in delovno obveznostjo zaposlenega v izbranem mesecu in letu'''
 
     razlika = oddelane_ure_za_posamezen_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto) - delovna_obveznost_dan(vnesen_id_zaposlenega, izbran_mesec, izbrano_leto)

31e5b5e27ea3985959c6c523b524b38e05164adc
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 18:21:27 +0200

    Refactor SQL queries and add tabela function

--- sql/statistika.py
+++ sql/statistika.py
@@ -1,76 +1,79 @@
-def poslovalnica_uporabnika(vpisan_id_uporabnika):
-  SELECT id_poslovalnice FROM zaposleni
-    WHERE id_zaposlenega = vpisan_id_uporabnika
+def tabela(izbrano_leto):
+  return f'prodaja{izbrano_leto}'
 
+def poslovalnica_uporabnika(vpisan_id_uporabnika):
+  sql = f"""SELECT id_poslovalnice FROM zaposleni
+    WHERE id_zaposlenega = vpisan_id_uporabnika
+"""
 poslovalnica = poslovalnica_uporabnika(vpisan_id_uporabnika)
     
 def sestevek_letne_prodaje(poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
+  prodaja = tabela(izbrano_leto)
   
-  SELECT SUM(prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
+  sql = f"""SELECT SUM(prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
-
+"""
 def top10(poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT izdelek, SUM(kolicina) FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT izdelek, SUM(kolicina) FROM prodaja
     WHERE id_poslovalnice = poslovalnica
       GROUP BY izdelek
         ORDER BY SUM(kolicina) DESC
           LIMIT 10
-
+"""
 def najslabsih10(poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT izdelek, SUM(kolicina) FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT izdelek, SUM(kolicina) FROM prodaja
     WHERE id_poslovalnice = poslovalnica
       GROUP BY izdelek
         ORDER BY SUM(kolicina) ASC
           LIMIT 10
-
+"""
 def mesecna_prodaja_v_izbranem_letu(poslovalnica, izbrano_leto):                    TREBA ŠE POPRAVIT
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) FROM prodaja
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
-  
+  """
 
 def nabavna_cena_v_letu((poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT SUM(prodaja.kolicina * izdelki.nabavna_cena) FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT SUM(prodaja.kolicina * izdelki.nabavna_cena) FROM prodaja
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
-
+"""
 def ustvarjen_profit(poslovalnica, izbrano_leto):
   return sestevek_letne_prodaje(poslovalnica, izbrano_leto) - nabavna_cena_v_letu((poslovalnica, izbrano_leto)
 
 def najboljsi_dan_prodaje(poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) AS dnevni_prihodek FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) AS dnevni_prihodek FROM prodaja
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
         GROUP BY datum
           ORDER BY dnevni_prihodek DESC
             LIMIT 1
-
+"""
 def najslabsi_dan_prodaje(poslovalnica, izbrano_leto):
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) AS dnevni_prihodek FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT datum, SUM(prodaja.kolicina * izdelki.prodajna_cena) AS dnevni_prihodek FROM prodaja
     JOIN izdelki ON izdelki.ime = prodaja.izdelek
       WHERE prodaja.id_poslovalnice = poslovalnica
         GROUP BY datum
           ORDER BY dnevni_prihodek ASC
             LIMIT 1
-
+"""
 def stevilo_prodaj(poslovalnica, izbrano_leto):  group by se je potrebno
-  prodaja = f'prodaja{izbrano_leto}'
-  SELECT COUNT(DISTINCT id_racuna) FROM prodaja
+  prodaja = tabela(izbrano_leto)
+  sql = f"""SELECT COUNT(DISTINCT id_racuna) FROM prodaja
     WHERE id_poslovalnice = poslovalnica
-
+"""
 def napoved_prodaje(poslovalnica, izbran_izdelek):                                          DODATI JE ŠE TREBA V KATERI POSLOVALNICI NAJ GLEDA
-  WITH leto2024 AS (SELECT SUM(kolicina) AS kolicina2024 FROM prodaja2024
+ sql =f""" WITH leto2024 AS (SELECT SUM(kolicina) AS kolicina2024 FROM prodaja2024
                       WHERE izdelek = izbran_izdelek),
        leto2025 AS (SELECT SUM(kolicina) AS kolicina2025 FROM prodaja2025
                       WHERE izdelek = izbran_izdelek)
   SELECT kolicina2024, kolicina2025, (kolicina2025 * (1 + (kolicina2025 - kolicina2024) / kolicina2024)) FROM leto2024
     CROSS JOIN leto2025
-  
+  """

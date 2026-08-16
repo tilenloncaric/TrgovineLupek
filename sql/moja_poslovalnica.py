528dfaff66a2d4ed353b9e516ae5ca117bf8e265
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 15:10:09 +0200

    Add database connection parameter to functions

--- sql/moja_poslovalnica.py
+++ sql/moja_poslovalnica.py
@@ -1,43 +1,84 @@
 import sqlite3
 
 
-def moja_poslovalnica(vnesen_id_zaposlenega):
+def moja_poslovalnica(vnesen_id_zaposlenega, pot_do_baze):
     '''prikaže podatke poslovalnice, kjer je zaposlen uporabnik'''
+
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor() 
 
     sql =   """ SELECT poslovalnice.ime, poslovalnice.delovni_cas, poslovalnice.postna_stevilka, poslovalnice.kraj, poslovalnice.naslov, poslovalnice.telefon FROM poslovalnice 
                     JOIN zaposleni ON zaposleni.id_poslovalnice = poslovalnice.id_poslovalnice
-                        WHERE zaposleni.id_zaposlenega = vnesen_id_zaposlenega 
+                        WHERE zaposleni.id_zaposlenega = ? 
             """
 
+    cursor.execute(sql, (vnesen_id_zaposlenega))
+    rezultat = cursor.fetchone()
 
-def poslovodja(vnesen_id_zaposlenega):
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+    return rezultat
+
+
+def poslovodja(vnesen_id_zaposlenega, pot_do_baze):
     '''izpiše ime in priimek poslovodje poslovalnice, kjer je zaposlen uporabnik'''
+
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor() 
 
     sql =   """ SELECT ime, priimek FROM zaposleni 
                     WHERE delovno_mesto = 'Poslovodja'
                         AND
                     id_poslovalnice = (SELECT id_poslovalnice FROM zaposleni 
-                                        WHERE id_zaposlenega = vnesen_id_zaposlenega)
+                                        WHERE id_zaposlenega = ?)
             """
 
+    cursor.execute(sql, (vnesen_id_zaposlenega))
+    rezultat = cursor.fetchone()
 
-def izmenovodje(vnesen_id_zaposlenega):
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+    return rezultat
+
+
+def izmenovodje(vnesen_id_zaposlenega, pot_do_baze):
     '''izpiše ime in priimek izmenovodij poslovalnice, kjer je zaposlen uporabnik'''
+
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor() 
 
     sql =   """ SELECT ime, priimek FROM zaposleni 
                     WHERE delovno_mesto = 'Izmenovodja'
                         AND
                     id_poslovalnice = (SELECT id_poslovalnice FROM zaposleni 
-                                        WHERE id_zaposlenega = vnesen_id_zaposlenega)
+                                        WHERE id_zaposlenega = ?)
             """
+   
+    cursor.execute(sql, (vnesen_id_zaposlenega))
+    rezultat = cursor.fetchall()                          # .fetchall() je za več vrstic
 
+    povezava_na_bazo.close()         # zapre povezavo z bazo
 
-def prodajalci(vnesen_id_zaposlenega):
+    return rezultat
+    
+
+def prodajalci(vnesen_id_zaposlenega, pot_do_baze):
     '''izpiše ime in priimek sodelavcev poslovalnice, kjer je zaposlen uporabnik'''
+
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor() 
 
     sql =   """ SELECT ime, priimek FROM zaposleni 
                     WHERE (delovno_mesto = 'Prodajalec' OR delovno_mesto = 'Prodajalka')
                         AND
                     id_poslovalnice = (SELECT id_poslovalnice FROM zaposleni 
-                                        WHERE id_zaposlenega = vnesen_id_zaposlenega)
+                                        WHERE id_zaposlenega = ?)
             """
+    
+    cursor.execute(sql, (vnesen_id_zaposlenega))
+    rezultat = cursor.fetchall()
+
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+    return rezultat
+    

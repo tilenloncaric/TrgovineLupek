fc36d81f7174a5f549d7a3e78d03b47ef79a9ee6
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 14:18:06 +0200

    Modify osebni_podatki to include database connection
    Updated the 'osebni_podatki' function to accept a database path and connect to the database.

--- sql/osebni_podatki.py
+++ sql/osebni_podatki.py
@@ -1,10 +1,22 @@
 import sqlite3
 
 
-def osebni_podatki(vnesen_id_zaposlenega):
+def osebni_podatki(vnesen_id_zaposlenega, pot_do_baze):
     '''prikaže osebne podatke zaposlenega'''
+
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor()
 
     sql =   """ SELECT zaposleni.ime, zaposleni.priimek, zaposleni.spol, zaposleni.delovno_mesto, poslovalnice.ime FROM zaposleni 
                     JOIN poslovalnice ON zaposleni.id_poslovalnice = poslovalnice.id_poslovalnice
-                        WHERE zaposleni.id_zaposlenega = vnesen_id_zaposlenega 
+                        WHERE zaposleni.id_zaposlenega = ? 
             """
+
+    
+    cursor.execute(sql, (vnesen_id_zaposlenega,))  # potrebna vejica, da se naredi tuple, sicer ne dela?????
+    rezultat = cursor.fetchone()
+
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
+
+    return rezultat

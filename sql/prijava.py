4a5e7ce45c17d24967c1a0a9cad95c38b9dd191a
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 14:12:42 +0200

    Refactor prijava.py for improved database handling
    Refactor login check to use parameterized queries and accept database path as an argument.

--- sql/prijava.py
+++ sql/prijava.py
@@ -1,26 +1,23 @@
 import sqlite3
-from pot import pot_do_mape
 
 
-pot_glavne_mape = pot_do_mape()                
-
-povezava_na_bazo = sqlite3.connect(f"{pot_glavne_mape}/baza/baza.db")      # ustvari povezavo z bazo
-kazalec = povezava_na_bazo.cursor()                                        # stvari kazalec za izvajanje SQL poizvedb
-
-
-def preveri_prijavo(vnesen_id_zaposlenega, vneseno_geslo):
+def preveri_prijavo(vnesen_id_zaposlenega, vneseno_geslo, pot_do_baze):
     '''preveri, ali je uporabnik vnesel pravilne podatke za prijavo'''
 
-    sql =   f""" SELECT * FROM zaposleni 
-                    WHERE id_zaposlenega = {vnesen_id_zaposlenega} 
-                        AND 
-                    geslo = '{vneseno_geslo}' 
-             """
+    povezava_na_bazo = sqlite3.connect(pot_do_baze)
+    cursor = povezava_na_bazo.cursor() 
 
-    kazalec.execute(sql)
-    rezultat = kazalec.fetchone()
+    sql = """ SELECT * FROM zaposleni 
+                WHERE id_zaposlenega = ? 
+                        AND 
+                      geslo = ? 
+          """
+
+    
+    cursor.execute(sql, (vnesen_id_zaposlenega, vneseno_geslo))
+    rezultat = cursor.fetchone()
+
+    povezava_na_bazo.close()         # zapre povezavo z bazo
+
 
     return rezultat
-
-
-povezava_na_bazo.close()         # zapre povezavo z bazo

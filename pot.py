46a09216f2b8ccff5ae5e0ec4703503d5bac1110
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-16 13:29:18 +0200

    Delete pot.py

--- pot.py
+++
@@ -1,7 +0,0 @@
-import os               # delo s datotekami in mapami
-
-def pot_do_mape():
-    '''funkcija, ki vrne absolutno pot mape TrgovineLupek, pot v kateri se nahaja naša datoteka'''
-    
-    return os.path.dirname(os.path.abspath(__file__))
-    

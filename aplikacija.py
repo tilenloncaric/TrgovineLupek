b7bfd152553c5920876319ae3d438be3a7ab2d2e
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-20 13:40:08 +0200

    Correct database path and remove unused connection
    Fixed path construction for database and removed redundant connection creation.

--- aplikacija.py
+++ aplikacija.py
@@ -3,13 +3,11 @@ import os
 # poglej če baza že obstaja, če ne jo ustvari sicer ne neradi nič
 glavna_mapa = os.path.dirname(os.path.abspath(__file__))     # mapa, kjer se nahaja trenutna .py datoteka
 
-pot_do_baze = os.path.join(trenutna_mapa,, baza "baza.db"
+pot_do_baze = os.path.join(trenutna_mapa, "baza", "baza.db"
 
 # če baza že obstaja, ne ustvarja nove baze in ne uvaža podatkov
 if not os.path.exists(pot_do_baze)):
-  
-  povezava_na_bazo = sqlite3.connect(os.path.join(trenutna_mapa, "baza.db"))       # ustvari povezavo na SQLite bazo (če ne obstaja, se ustvari nova)
-  cursor = povezava_na_bazo.cursor()   
+  ustvari_bazo_in_uvozi_podatke(pot_do_baze)
 
 # prijava uporabnika, preveri če obstaja
 

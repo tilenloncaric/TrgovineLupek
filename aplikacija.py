4946a9200a2b05ff9281e9de328a649f53bb4ed4
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-20 13:33:42 +0200

    Add comments for clarity in aplikacija.py
    Added comments to explain the application structure and functionality.

--- aplikacija.py
+++ aplikacija.py
@@ -1,1 +1,10 @@
-fenfunhvunh
+
+
+# poglej če baza že obstaja, če ne jo ustvari sicer ne neradi nič
+
+# prijava uporabnika, preveri če obstaja
+
+# začetna stran, ki pove dobrodošli v trgovine lupek, potem so zgoaj zavihtki na katere lahko uporabnik stisne
+
+# če stisne zavihek Osebna evdenca - izpiše se osebna evidenca, Statistika - pokaže se možnost izbire kakšna statistika ga zanima
+

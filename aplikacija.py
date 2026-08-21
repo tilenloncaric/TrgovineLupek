a9eab6fb0a44e50386155d182cdaf64fef82cba2
Tilen Lončarič <125560244+tilenloncaric@users.noreply.github.com>
2026-08-21 12:29:47 +0200

    Update aplikacija.py

--- aplikacija.py
+++ aplikacija.py
@@ -1,4 +1,5 @@
 import os
+from baza import uvoz
 from sql import prijava    # klic funkcije delaš kot prijava.preveri_prijavo()
 from sql import osebni_podatki
 from sql import moja_poslovalnica
@@ -8,14 +9,15 @@ from sql import sprememba
 from sql import statistika
 from flask import Flask, render_template, request, session
 
-# poglej če baza že obstaja, če ne jo ustvari sicer ne neradi nič
-glavna_mapa = os.path.dirname(os.path.abspath(__file__))     # mapa, kjer se nahaja trenutna .py datoteka
 
+# ustvarjanje baze
+glavna_mapa = os.path.dirname(os.path.abspath(__file__))     # mapa, kjer se nahaja trenutna .py datoteka
 pot_do_baze = os.path.join(trenutna_mapa, "baza", "baza.db"
 
 # če baza že obstaja, ne ustvarja nove baze in ne uvaža podatkov
 if not os.path.exists(pot_do_baze)):
-  ustvari_bazo_in_uvozi_podatke(pot_do_baze)
+  uvoz.ustvari_bazo_in_uvozi_podatke(pot_do_baze)
+
 
 
 # ustvari flask aplikacijo
@@ -23,39 +25,46 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def prikazi_prijavo():
+def stran_prijava_v_aplikacijo():
   '''odpre spletno stran za prijavo uporabnika'''
   return render_template("prijava.html")
 
 
-@app.route("/prijava", methods=["POST"])
-def prijava():
+
+@app.route("/prijava", methods=["POST"])     # sem na spletni strani prijava.html, 
+def stran_prijava():
   '''zahteva podatke za prijavo'''
 
   # zahteva vnos uporanika
   uporabniska_stevilka = request.form.get("uporabniska_stevilka")
   geslo = request.form.get("geslo")
 
-  # izvede se SQL poizvedba
-  sql_preveri_prijavo = prijava.preveri_prijavo(uporabniska_stevilka, geslo)
+  # izvede se SQL poizvedba, ki preveri ali v bazi podatkov obstaja uporabnik z vvnešenim uporabniškim imenom in geslom
+  preveri_prijavo = prijava.preveri_prijavo(uporabniska_stevilka, geslo)
 
   # če je prijava uspešna se odpre začetna stran, sicer se odpre stran, ki javi napako
-  if sql_preveri_prijavo:
+  if preveri_prijavo:
     session["uporabniska_stevilka"] = uporabniska_stevilka   # da je vnesena uporabniška številka vidna vsem uporabnikom
     return render_template("zacetna_stran.html")
   return render_template("napaka_v_prijavi.html")
 
 
+
 @app.route("/osebni_podatki")
-def osebni_podatki():
-  uporabniska_stevilka = session.get(session.get())
-  ime, priimek, spol, delovno_mesto, poslovalnica = osebni_podatki.osebni_podatki(uporabniska_stevilka)
+def stran_osebni_podatki():
+  '''izpis osebnih podatkov na spletni strani osebni_podatki.html'''
+  
+  uporabniska_stevilka = session.get("uporabniska_stevilka")
+  ime, priimek, spol, delovno_mesto, poslovalnica = osebni_podatki.osebni_podatki(uporabniska_stevilka, pot_do_baze)
+  
   return render_template("osebni_podatki.html", ime=ime, priimek=priimek, delovno_mesto=delovno_mesto, poslovalnice=poslovalnica)
 
 
+
 @app.route("/moja_poslovalnica")
-def moja_poslovalnica():
-  uporabniska_stevilka = session.get(session.get())
+def stran_moja_poslovalnica():
+  ''''''
+  uporabniska_stevilka = session.get("uporabniska_stevilka")
   return render_template("moja_poslovalnica.html")
 
 
